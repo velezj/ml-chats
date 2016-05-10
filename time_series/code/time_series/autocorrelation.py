cf46f3e1d4e5a2fc1e8aad60f0f50e638e2b2840
Javier Velez <velezj@alum.mit.edu>
2016-05-09 21:15:40 -0400

    Fixes to period estimation, including not looking at more than half the signal length

--- time_series/code/time_series/autocorrelation.py
+++ time_series/code/time_series/autocorrelation.py
@@ -23,14 +23,15 @@ def autocorrelation_estimate( x ):
 ##
 # Estimate the period of a sequence using the autocorrelation.
 # Returns a distirbution over the period (discrete)
-def period_estimate( x, prior = cat_distibution() ):
+def period_estimate( x, prior = cat_distibution({}) ):
 
     # first, calculate autocorrelation
     ac = autocorrelation_estimate( x )
 
     # Ok, now ignore first datapoint and take the top 5 percent of the
     # height
-    height_thresh = 2 * np.std( ac[1:] )
+    n = len(x)
+    height_thresh = 2 * np.std( ac[1:n/2] )
 
     # now find continuous ranges with value above the threhold
     ranges = [] # [start,end) intervals
@@ -50,15 +51,19 @@ def period_estimate( x, prior = cat_distibution() ):
             # stop range if we were accruing one
             if current_start is not None:
                 ranges.append( (current_start, idx ) )
+                current_start = None
 
     # Ok, if we have no ranges then there is no period
-    if len(ranges) == 0:
+    if len(ranges) < 2:
         return cat_distibution( { 0: 1.0 } )
+
+    # ok, grab the midpoint of each range
+    mids = map(lambda (a,b): a + (b-a) / 2.0 , ranges )
 
     # Ok, calculate probability for different period lengths
     counts = {}
-    for r in ranges:
-        k = r[1] - r[0]
+    for a,b in zip(mids,mids[1:]):
+        k = b - a
         if k not in counts:
             counts[ k ] = 0.0
         counts[ k ] += 1.0
